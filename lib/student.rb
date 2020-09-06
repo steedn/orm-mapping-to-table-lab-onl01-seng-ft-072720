da0e44cbf1218b78bd3a30dab51f6dd4f818d3e5
Nathan Steed <steedn@outlook.com>
2020-09-06 08:43:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,7 @@ class Student
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end
  def self.create(name)
+   binding.pry
   #  student = Student.new(name, grade)
    student.save
    student
