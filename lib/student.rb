cb6256a8a4e02e96c2bb656ff22d0eb07e62d56c
Nathan Steed <steedn@outlook.com>
2020-09-06 08:30:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
-  attr_accessor :name, :grade, :id 
+  attr_accessor :name, :grade, :id
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
-    @id = id 
+    @id = id
   end
 end
